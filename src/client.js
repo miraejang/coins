2590c7e2146933b1f71db50ea473bda7342701df
miraejang <mirae611@nate.com>
2022-12-26 07:00:22 +0900

    client 수정

--- src/client.js
+++ src/client.js
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  // uri: 'https://apollo-movies-app.herokuapp.com/',
-  uri: 'http://localhost:4000/',
+  uri: 'https://apollo-movies-app.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
